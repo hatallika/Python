bc20fc45b1346802d8cb578a033f79ce8f63438f
Hatallika <essenadmin@gmail.com>
2023-07-02 18:20:10 +0300

    lesson6 strings methods

---
+++ Lesson6_1.py
@@ -0,0 +1,44 @@
+# word = 'apple word'
+# print(word.replace('pp', 'll'))
+# print(word)
+# print(word.strip('e'))
+# print(word.lstrip('a'))
+# print(word.find('kk'))
+# if word.find('pp') != -1:
+#     print('Строка есть')
+# else:
+#     print('Нет строки')
+word = 'hello'
+# print(word.split())
+# word_list = list(word)
+# print(word_list)
+# word_list[3] = 'f'
+# print(word_list)
+# new_word_list = str(word_list)
+# print(type(new_word_list))
+# new_word = ''.join(word_list)
+# print(new_word)
+# print(word.__dir__())
+# print(help(print()))
+# a = '''Привет привет привет
+#         Привет привет'''
+# print(a)
+
+# def main():
+#     """Hello hello hello"""
+#     pass
+# print(main.__doc__)
+# name = 'Василий'
+# isTrue = True
+# while isTrue:
+#     change_name = input('Укажите какую подстроку вы хотите изменить и на какую через пробел: ').split()
+#     if name.find(change_name[0]) != -1:
+#         new_name = name.replace(change_name[0], change_name[1])
+#         print(new_name)
+#         isTrue = False
+#     else:
+#         print('Такой подстроки нет')
+# print(new_name is name)
+# print(id(new_name), id(name))
+
+print(ord('a'))
